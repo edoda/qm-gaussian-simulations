be9e8c0dbd4fe4b70da59ef9804ba7238b1dc35f
Emilia Doda <edoda@usc.edu>
2025-01-06 21:33:10 -0800

    moved public to frontend root

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 
 //optional? external scripts (Plotly and Pyodide)
@@ -23,7 +23,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Theme accentColor="tomato" radius="full">
       <App />
-      <ThemePanel />
+      {/* <ThemePanel /> */}
     </Theme>
   </React.StrictMode>
 );
